bf37d730a0273d3f56aeffefbc29eac94c378d46
Sergey Lebedev <londeren@gmail.com>
2013-11-12 10:53:01 +0400

    Вывод постов в архиве

--- www/detail.php
+++ www/detail.php
@@ -10,13 +10,14 @@ require('./Post.php');
 
 $post = new Post($_GET['id']);
 
+$postArchive = $post->getArchive();
 
 ?><!doctype html>
 <html lang="ru-RU">
 <head>
   <meta charset="UTF-8">
   <title><?=$post->params['name'];?></title>
-  <link rel="stylesheet" href="/includes/admin.css" />
+  <link rel="stylesheet" href="/includes/style.css" />
 </head>
 <body>
 <header>
@@ -25,17 +26,33 @@ $post = new Post($_GET['id']);
 
 <section>
   <article>
-    <?= $post->params['message']; ?>
+    <p><?= $post->params['message']; ?></p>
   </article>
   <aside>
     <nav>
       <ul>
         <li><a href="/">Последние посты</a></li>
       </ul>
+      <?php
+      if(!empty($postArchive))
+      {
+        ?>
+        <h2>Архив постов</h2>
+        <ul>
+          <?php
+          foreach($postArchive as $archiveItem)
+          {
+          ?><li><a href="archive.php?year=<?=$archiveItem['year'];?>&month=<?=$archiveItem['month'];?>"><?=$archiveItem['month_name'];?> <?=$archiveItem['year'];?></a></li><?
+          }
+          ?>
+        </ul>
+      <?
+      }
+      ?>
     </nav>
   </aside>
 </section>
-
+<div class="b_m"></div>
 <footer>&copy; <a href="http://cetera.ru">Cetera labs</a> 2013</footer>
 </body>
 </html>
